75adaa81e6a4eedfa6790fc7c603f2e4766e67d8
sainath161 <sainathsmart1234@gmail.com>
2024-05-24 18:41:13 +0530

    Real Estate Site

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnKQh07v6xnWF1LtaILeDNSSxwTeU3lRs",
+  authDomain: "estate-4525d.firebaseapp.com",
+  projectId: "estate-4525d",
+  storageBucket: "estate-4525d.appspot.com",
+  messagingSenderId: "746404035069",
+  appId: "1:746404035069:web:e68b738befb7e4aede0194",
+  measurementId: "G-7HQSPELMET",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
